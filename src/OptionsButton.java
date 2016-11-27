50b8dff3fda5cacc2ed75c902c7ed0a233225a21
Łukasz Szcześniak <sir.viters@gmail.com>
2016-11-27 21:41:18 +0100

    OptionsButton is now static builder.

--- src/OptionsButton.java
+++ src/OptionsButton.java
@@ -4,14 +4,11 @@ import javax.swing.border.EmptyBorder;
 /**
  * Created by sir.viters on 26.11.2016.
  */
-class OptionsButton extends JButton {
-    OptionsButton(String text) {
-        super(text);
-        setup();
-    }
-
-    private void setup() {
-        setFocusable(false);
-        setBorder(new EmptyBorder(5, 10, 5, 10));
+class OptionsButton {
+    static JButton createButton(String label) {
+        return new JButton(label) {{
+            setFocusable(false);
+            setBorder(new EmptyBorder(5, 10, 5, 10));
+        }};
     }
 }
